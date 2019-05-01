46065843c67c107c91a62c4397bc570446ec732c
avvinci <vinciabhinav7@gmail.com>
2019-05-01 13:28:56 +0530

    auto image update added :)

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-const numItemsToGenerate = 10; //how many gallery items you want on the screen
+const numItemsToGenerate = 2; //how many gallery items you want on the screen
 const numImagesAvailable = 70; //how many total images are in the collection you are pulling from
 const imageWidth = 480; //your desired image width in pixels
 const imageHeight = 480; //desired image height in pixels
@@ -6,22 +6,27 @@ const collectionID = 2565034; //the collection ID from the original url
 const galleryContainer = document.querySelector('.gallery-container');
 let keywords = ['paris','music','books'];
 
+let counter = 0;  
+
 function renderGalleryItem(randomNumber){
-  let url = `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}` ; 
-  // let url2 = `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?${keywords[2]}`;
-  
+  let galleryItem = document.createElement('div');
+  galleryItem.classList.add('gallery-item');
+  let imgID = 'imgID'+ counter ; 
+  galleryItem.setAttribute('id' , imgID) ; 
+  galleryContainer.appendChild(galleryItem);
+  counter++ ; 
+
+  let url = `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}` ;   
   fetch(url) 
     .then((response)=> {    
-      let galleryItem = document.createElement('div');
-      galleryItem.classList.add('gallery-item');
       // console.log(response.url) ; 
       galleryItem.innerHTML = `
         <img class="gallery-image" src="${response.url}" alt="gallery image"/>
       `
-      galleryContainer.appendChild(galleryItem);
     })
-    
+
 }
+
 
 function getImage(){
     for(let i=0;i<numItemsToGenerate;i++){
@@ -30,3 +35,30 @@ function getImage(){
         renderGalleryItem(randomImageIndex);
       }
 }
+
+function updateGalleryItem(i,randomNumber){
+  console.log('updating' , i);
+  let imgID = 'imgID'+ i ; 
+  let galleryItem = document.getElementById(imgID);
+
+  let url = `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}` ; 
+  fetch(url)
+  .then((response) => {
+    galleryItem.innerHTML = `
+    <img class="gallery-image" src="${response.url}" alt="gallery image"/>
+    `
+  })
+
+}
+
+function updateGallery(){
+  // counter = 0 ; 
+  for(let i=0;i<numItemsToGenerate;i++){
+    let randomImageIndex = Math.floor(Math.random() * numImagesAvailable); 
+    updateGalleryItem(i,randomImageIndex);
+  }
+}
+
+window.onload = getImage ; 
+
+setInterval(updateGallery, 5000);
