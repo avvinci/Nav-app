5170224d7f599a4164e9ea20ec86528f5a59e92f
avvinci <vinciabhinav7@gmail.com>
2019-05-01 12:03:53 +0530

    barebones working

--- scripts.js
+++ scripts.js
@@ -1,26 +1,32 @@
 const numItemsToGenerate = 10; //how many gallery items you want on the screen
-const numImagesAvailable = 242; //how many total images are in the collection you are pulling from
+const numImagesAvailable = 70; //how many total images are in the collection you are pulling from
 const imageWidth = 480; //your desired image width in pixels
 const imageHeight = 480; //desired image height in pixels
-const collectionID = 540518; //the collection ID from the original url
-const $galleryContainer = document.querySelector('.gallery-container');
+const collectionID = 2565034; //the collection ID from the original url
+const galleryContainer = document.querySelector('.gallery-container');
+let keywords = ['paris','music','books'];
 
 function renderGalleryItem(randomNumber){
-  fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}`) 
+  let url = `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}` ; 
+  // let url2 = `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?${keywords[2]}`;
+  
+  fetch(url) 
     .then((response)=> {    
       let galleryItem = document.createElement('div');
       galleryItem.classList.add('gallery-item');
-      console.log(response.url) ; 
+      // console.log(response.url) ; 
       galleryItem.innerHTML = `
         <img class="gallery-image" src="${response.url}" alt="gallery image"/>
       `
-      $galleryContainer.appendChild(galleryItem);
+      galleryContainer.appendChild(galleryItem);
     })
+    
 }
 
 function getImage(){
     for(let i=0;i<numItemsToGenerate;i++){
         let randomImageIndex = Math.floor(Math.random() * numImagesAvailable);
-        renderGalleryItem(i);
+        // console.log(randomImageIndex) ; 
+        renderGalleryItem(randomImageIndex);
       }
 }
