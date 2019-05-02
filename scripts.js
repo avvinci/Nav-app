1a0a602e6d2bb27b2cbe42c4eff092ff94b141d2
avvinci <vinciabhinav7@gmail.com>
2019-05-02 13:50:40 +0530

    all buttons functional

--- scripts.js
+++ scripts.js
@@ -1,25 +1,52 @@
 const numItemsToGenerate = 5; //how many gallery items you want on the screen
-const numItemsToUpdate = 2 ; //how many gallery items you want on the screen
+const numItemsToUpdate = 2 ; //how many gallery items you want to update the screen
 const numImagesAvailable = 70; //how many total images are in the collection you are pulling from
 const imageWidth = 480; //your desired image width in pixels
 const imageHeight = 480; //desired image height in pixels
-const collectionID = 858095; //the collection ID from the original url
-let themeCollectionID = [159165,148987,1236,208403,148984] ; 
+
+let collectionID = 858095; //the collection ID from the unsplash 
+let themeCollectionID = [3356576,3356570,858095,148987,3330455] ; 
 let keywords = ['paris','music','books'];
 
 const galleryContainer = document.querySelector('.gallery-container');
-let button4 = document.getElementById('button4') ; 
 
-button4.onclick = function(){
+let button0 = document.getElementById('button0') ; 
+button0.onclick = function(){
+  collectionID = themeCollectionID[0] ; 
+  updateGallery(numItemsToGenerate); 
+};
+
+let button1 = document.getElementById('button1') ; 
+button1.onclick = function(){
+  collectionID = themeCollectionID[1] ; 
+  updateGallery(numItemsToGenerate); 
+};
+
+let button2 = document.getElementById('button2') ; 
+button2.onclick = function(){
+  collectionID = themeCollectionID[2] ; 
+  updateGallery(numItemsToGenerate); 
+};
+
+
+let button3 = document.getElementById('button3') ; 
+button3.onclick = function(){
   collectionID = themeCollectionID[3] ; 
-  updateGallery(); 
-} ; 
+  updateGallery(numItemsToGenerate); 
+};
+
+let button4 = document.getElementById('button4') ; 
+button4.onclick = function(){
+  collectionID = themeCollectionID[4] ; 
+  updateGallery(numItemsToGenerate); 
+};
+
+
 window.onload = getImageGallery ; 
 
 // function updateGalleryCollection(newid){
  
 // }
-
 
 function setImageItem(galleryItem, randomNumber){
   let url = `https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}/?sig=${randomNumber}` ;   
@@ -59,8 +86,12 @@ function updateGalleryItem(i,randomNumber){
   setImageItem(galleryItem, randomNumber) ; 
 }
 
-function updateGallery( ){
-  for(let i=0;i<numItemsToUpdate;i++){
+function updateGallery(numofItems = 0 ){
+  let count = numItemsToUpdate ; 
+  if(numofItems !== 0 ) {
+    count = numofItems;
+  }
+  for(let i=0;i<count;i++){
     let randomImageIndex = Math.floor(Math.random() * numImagesAvailable); 
     updateGalleryItem(i,randomImageIndex);
   }
